1885d1145a3f9c86f88bad91f78fe668e0d2c1dc
Frank Henard <fdhenard@gmail.com>
2017-01-17 16:43:16 -0600

    cleanup

--- testproject/testproject/tests/model_to_dict_tests.py
+++ testproject/testproject/tests/model_to_dict_tests.py
@@ -2,6 +2,8 @@ import unittest
 
 from testproject.models import Parent
 
+import django_serialize
+
 
 class ModelToDictTests(unittest.TestCase):
 
@@ -9,12 +11,6 @@ class ModelToDictTests(unittest.TestCase):
         pobj = Parent.objects.create(
             some_char_property='testing',
         )
-        # import os, inspect
-        # currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        # parentdir = os.path.dirname(currentdir)
-        import django_serialize
+        self.assertEqual(pobj.some_char_property, 'testing')
         res = django_serialize.model_to_dict(pobj)
         import pprint; pprint.pprint(res)
-        # import pprint, sys; pprint.pprint(sys.path)
-        # import pdb; pdb.set_trace()
-        self.assertEqual(pobj.some_char_property, 'testing')
