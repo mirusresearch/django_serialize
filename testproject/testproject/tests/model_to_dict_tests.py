dce90b6bb92e6c9043c5a9e1b217cb0baf0f4e3b
Frank Henard <fdhenard@gmail.com>
2017-01-17 16:45:08 -0600

    improve simple test

--- testproject/testproject/tests/model_to_dict_tests.py
+++ testproject/testproject/tests/model_to_dict_tests.py
@@ -7,10 +7,11 @@ import django_serialize
 
 class ModelToDictTests(unittest.TestCase):
 
-    def test_it(self):
+    def test_simple(self):
         pobj = Parent.objects.create(
             some_char_property='testing',
         )
         self.assertEqual(pobj.some_char_property, 'testing')
         res = django_serialize.model_to_dict(pobj)
-        import pprint; pprint.pprint(res)
+        self.assertEqual(res['some_char_property'], 'testing')
+        # import pprint; pprint.pprint(res)
