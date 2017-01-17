2080bd4a6f35088f35526b3376b3aa2ea12d22ec
Frank Henard <fdhenard@gmail.com>
2017-01-17 16:51:30 -0600

    testing for one to one field

--- testproject/testproject/tests/model_to_dict_tests.py
+++ testproject/testproject/tests/model_to_dict_tests.py
@@ -1,11 +1,17 @@
 import unittest
 
-from testproject.models import Parent
+from testproject.models import Parent, ChildOneToOne, ChildManyToMany, ChildOneToMany
 
 import django_serialize
 
 
 class ModelToDictTests(unittest.TestCase):
+
+    def setUp(self):
+        ChildOneToOne.objects.all().delete()
+        ChildManyToMany.objects.all().delete()
+        ChildOneToMany.objects.all().delete()
+        Parent.objects.all().delete()
 
     def test_simple(self):
         pobj = Parent.objects.create(
@@ -14,4 +20,16 @@ class ModelToDictTests(unittest.TestCase):
         self.assertEqual(pobj.some_char_property, 'testing')
         res = django_serialize.model_to_dict(pobj)
         self.assertEqual(res['some_char_property'], 'testing')
+        # import pprint; pprint.pprint(res)
+
+    def test_another(self):
+        coto = ChildOneToOne.objects.create(
+            some_int_property=1,
+        )
+        pobj = Parent.objects.create(
+            some_char_property='testing',
+            a_one_to_one_child=coto,
+        )
+        res = django_serialize.model_to_dict(pobj)
+        self.assertEqual(res['a_one_to_one_child']['some_int_property'], 1)
         # import pprint; pprint.pprint(res)
